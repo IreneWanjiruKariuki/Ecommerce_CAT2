8629c831bee315f8fb5d2852f9641af789d78b9a
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2024-11-26 16:31:18 +0300

    second commit

--- my_ecommerce/shop/models.py
+++ my_ecommerce/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+  fname = models.CharField(max_length=100)
+  lname = models.CharField(max_length=100)
+  phone = models.CharField(max_length=15)
+  email = models.EmailField(unique=True)
+  address = models.TextField(blank=True,null=True)
+
+class Order(models.Model):
+  customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+  quantity_ordered = models.IntegerField()
+  order_date = models.DateTimeField(auto_now_add=True)
+  total_amount = models.DecimalField(max_digits=10, decimal_places=2)
--- my_ecommerce/shop/views.py
+++ my_ecommerce/shop/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def shop(request):
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
